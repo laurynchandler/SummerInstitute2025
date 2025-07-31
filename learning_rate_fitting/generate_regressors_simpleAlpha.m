0476d53e46a526fe605be2b623267df79854e00c
Ari Khoudary <makhouda@uci.edu>
2025-07-31 11:52:51 -0700

    update code after aaron mtg; scaled alpha not working yet, simple alpha returning lots of floor/ceiling fits

--- learning_rate_fitting/generate_regressors_simpleAlpha.m
+++ learning_rate_fitting/generate_regressors_simpleAlpha.m
@@ -1,4 +1,4 @@
-function [regressor_matrix, prediction_errors]=generate_regressors_simpleRW(sub_data, alpha)
+function [regressor_matrix, prediction_errors]=generate_regressors_simpleAlpha(sub_data, alpha)
 %%%% INPUTS:
 % sub_data: a single subject's data from learning
 % params: list of values to be optimized
@@ -6,25 +6,43 @@ function [regressor_matrix, prediction_errors]=generate_regressors_simpleRW(sub_
 % regressor matrix: variables to use as regressors when fitting trial-level RTs
 % prediction errors: the inferred prediction error on each trial
 
+%% set up matrices to store variables
 maxTrials = height(sub_data);
 transition_matrix = ones(3, 2)*0.5; % rows correspond to cues, columns to targets
 counter_matrix = zeros(3, 2); % stores the number of times each outcome is observed for each cue
 prediction_errors = zeros(maxTrials, 1);
 counterfactual_updates = zeros(maxTrials, 1);
-regressor_matrix = zeros(maxTrials, 3);
+same_cue_transition = zeros(maxTrials, 1);
+same_image_transition = zeros(maxTrials, 1);
+trial_prediction = zeros(maxTrials, 1);
+regressor_matrix = zeros(maxTrials, 4);
 
+%% do cue learning
+alpha  = 1./(1 + exp(-alpha)); 
 for i=1:maxTrials
     cue = sub_data.cueIdx(i);
     image = sub_data.imageIdx(i);
     counter_matrix(cue, image) = counter_matrix(cue, image) + 1;
 
+    trial_prediction(i) = transition_matrix(cue, image);
     prediction_errors(i) = 1 - transition_matrix(cue, image);
     counterfactual_updates(i) = 0 - transition_matrix(cue, 3-image); % update probabilities for unobserved image
 
     transition_matrix(cue, image) = transition_matrix(cue, image) + alpha * prediction_errors(i);
     transition_matrix(cue, 3-image) = transition_matrix(cue, 3-image) + alpha * counterfactual_updates(i);
+
+    if i > 1
+        if cue == sub_data.cueIdx(i-1)
+            same_cue_transition(i) = 1;
+        end
+        if image == sub_data.imageIdx(i-1)
+            same_image_transition(i) = 1;
+        end
+    end
 end
 
+%% store values
 regressor_matrix(:,1) = 1;
-regressor_matrix(:,2) = prediction_errors;
+regressor_matrix(:,2) = trial_prediction;
+regressor_matrix(:, 3) = same_cue_transition;
 regressor_matrix(:,end) = 1:length(regressor_matrix);
--- learning_rate_fitting/optimize_fits.m
+++ learning_rate_fitting/optimize_fits.m
@@ -8,14 +8,11 @@
 
 %% load in data
 if (~exist('datadir', 'var'))
-    datadir='data/';
+    datadir='../data/';
 end
 
 % load in everyone's data
 behav_data = readtable([datadir 'learning.csv']);
-
-% define output directory
-outdir = 'fits/';
 
 % create table to store results
 nrow = length(unique(behav_data.subID));
@@ -29,42 +26,50 @@ options  = optimset('Algorithm', 'interior-point', 'MaxFunEvals', 1e5, 'MaxIter'
 
 % step size during optimization
 gridstep = 0.05;
-search_space  = [-1-gridstep 1+gridstep];     % initial values for the model parameters
+search_space  = [-gridstep 1+gridstep];     % initial values for the model parameters
 
-%% iterate through subjects
-for sub = unique(behav_data.subID)'
+%% iterate through alpha types & subjects
+fit_functions = {'generate_regressors_simpleAlpha', 'generate_regressors_scaledAlpha'};
 
-    % subset to just this subject's data
-    sub_data = behav_data(behav_data.subID == sub, :);
-    sub_data = sub_data(~isnan(sub_data.imgLockedRT), :);
-    fits.subID(sub) = sub;
-    loglikelihood = Inf;
+for i = 2:length(fit_functions)
+    fitfunc = fit_functions{i};
+    for sub = unique(behav_data.subID)'
+        % subset to just this subject's data
+        sub_data = behav_data(behav_data.subID == sub, :);
+        sub_data = sub_data(~isnan(sub_data.imgLockedRT), :);
+        fits.subID(sub) = sub;
+        loglikelihood = Inf;
 
-    % define anonymous function for estimating alpha jointly using RTs & observed transitions
-    thisfit = @(x) fit_function('generate_regressors_simpleRW', sub_data, x);
-    disp(['Fitting subject ' num2str(sub) '...']);
+        % define anonymous function for estimating alpha jointly using RTs & observed transitions
+        thisfit = @(x) fit_function(fitfunc, sub_data, x);
+        disp(['Fitting subject ' num2str(sub) '...']);
 
-    % search through parameter space
-    for k = min(search_space):gridstep:max(search_space)
-        alpha = k;
-        try
-            [temp_best_params, temp_loglik] = fmincon(thisfit, alpha, [], [], [], [], -Inf, Inf, [], options);
-        catch xcept
-            disp('fmincon error');
-            xcept
-            temp_best_params = alpha;
-            temp_loglik = loglikelihood;
+        % search through parameter space
+        for k = min(search_space):gridstep:max(search_space)
+            alpha = k;
+            try
+                if i==1
+                    [temp_best_params, temp_loglik] = fmincon(thisfit, alpha, [], [], [], [], -Inf, Inf, [], options);
+                else
+                    params = [alpha_0 alpha_slope];
+                    [temp_best_params, temp_loglik] = fmincon(thisfit, params, [], [], [], [], -Inf, Inf, [], options);
+                end
+            catch xcept
+                disp('fmincon error');
+                xcept
+                temp_best_params = alpha;
+                temp_loglik = loglikelihood;
+            end
+            if (temp_loglik < loglikelihood-(1e-32))
+                temp_best_params(1) = 1./(1 + exp(-temp_best_params));   % back-transform alpha from logistic
+                disp(['Subject ' num2str(sub) ', new best parameters: ' num2str(temp_best_params)]);
+                fits.fitted_alpha(sub) = temp_best_params;
+                fits.("-loglikelihood")(sub) = temp_loglik;
+            end
         end
-        if (temp_loglik < loglikelihood-(1e-32))
-            %temp_best_params(1) = 1./(1 + exp(-temp_best_params(1));            % Convert omega to alpha
-            disp(['Subject ' num2str(sub) ', new best parameters: ' num2str(temp_best_params)]);
-            fits.fitted_alpha(sub) = temp_best_params;
-            fits.("-loglikelihood")(sub) = temp_loglik;
-            %temp_loglik
-        end
-    end
-end % for each subject
-
-%% tidy & write out fits
-fits = rmmissing(fits);
-writetable(fits, 'first_fits.csv');
+    end % for each subject
+    %% tidy & write out fits
+    fits = rmmissing(fits);
+    fits = fits(fits.subID > 0, :);
+    writetable(fits, [extractAfter(fitfunc, 'generate_regressors_') '_fits.csv']);
+end % for each form of alpha
